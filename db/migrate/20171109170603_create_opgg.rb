dc158ac5e97cab781ed27d3851a293cebb8da0f1
edwardgemson <edwardgemson@gmail.com>
2017-11-09 17:10:31 -0500

    add timestamps to migrations

--- db/migrate/20171109170603_create_opgg.rb
+++ db/migrate/20171109170603_create_opgg.rb
@@ -4,6 +4,7 @@ class CreateOpgg < ActiveRecord::Migration[5.1]
     	t.string :user_id, null: false
     	t.string :champions, null: false
     	t.string :recommendation, null: false
+    	t.timestamps null: false
     end
   end
 end
